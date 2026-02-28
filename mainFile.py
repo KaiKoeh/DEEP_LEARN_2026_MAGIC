da9cb9acc5961713c190b83fbc425715a5d8ff40
MrKoiKoi <MrKoiKoi>
2026-02-28 15:55:55 +0100

    no message

--- mainFile.py
+++ mainFile.py
@@ -11,8 +11,12 @@ pic_target_resolution=256    #### Target Größe!!!
 
 pic_scale_factor = pic_main_resolution/pic_target_resolution
 
-##target_folder = "/Users/kaikohrsen/Documents/schulung/PythonWeekly/deep_learn_project/final_data"
-target_folder = r"C:\Users\MrKoiKoi\PycharmProjects\PythonProject\EndProjekt\final_data"
+
+### MAC
+##target_folder = "/Users/kaikohrsen/Documents/schulung/PythonWeekly/deep_learn_project/final_data/train_data_real"
+
+### PC
+target_folder = r"C:\Users\MrKoiKoi\PycharmProjects\PythonProject\EndProjekt\final_data\train_data"
 
 
 
@@ -67,14 +71,6 @@ def load_data(folder):
 
     return images, y_bbox, y_class, label_names
 
-
-
-
-
-
-
-
-
 ##### LADEN DER DATEN
 images, y_bbox, y_class, label_names = load_data(target_folder)
 label_amount = len(label_names)
@@ -110,18 +106,16 @@ plt.show()
 
 X_train, X_test, y_train_bbox, y_test_bbox, y_train_class, y_test_class  = train_test_split(images, y_bbox, y_class, test_size=0.2, random_state=240)
 
-base_model = keras.applications.MobileNet(
-    input_shape=None,
+base_model = keras.applications.MobileNetV2(
+    input_shape=(pic_target_resolution, pic_target_resolution, 3),
     alpha=1.0,
-    depth_multiplier=1,
-    dropout=0.001,
     include_top=False,
     weights="imagenet",
     input_tensor=None,
     pooling=None,
     classes=1000,
-    classifier_activation="softmax",
-    name=None)
+    classifier_activation="softmax"
+)
 
 
 ### WICHTIG
@@ -131,7 +125,7 @@ base_model.trainable = False
 
 
 def my_preprocess(x, training):
-    return keras.applications.mobilenet.preprocess_input(x)
+    return keras.applications.mobilenet_v2.preprocess_input(x)
 
 
 ## MODEL INIT
@@ -141,24 +135,33 @@ base_input = keras.layers.Input((pic_target_resolution, pic_target_resolution, 3
 seq_model.add(base_input)
 
 seq_model.add(keras.layers.RandomBrightness(0.2, value_range=(0, 255)))
+seq_model.add(keras.layers.RandomContrast(0.2, value_range=(0, 255)))
 
 seq_model.add(keras.layers.Lambda(my_preprocess))
+
+### ADD BASE MODEL
 seq_model.add(base_model)
 
+# Output > Teilung auf Klasse und BBox
+shared = seq_model.outputs[0]
 
 
-seq_model.add(keras.layers.Dropout(0.25))
-seq_model.add(keras.layers.Dense(512, activation='relu'))
-seq_model.add(keras.layers.Dropout(0.25))
-seq_model.add(keras.layers.Dense(256, activation='relu'))
-seq_model.add(keras.layers.Dropout(0.25))
-seq_model.add(keras.layers.Dense(128, activation='relu'))
+# Verzweigung für zwei Outputs: Klasse && BBox
 
-avg = keras.layers.GlobalAveragePooling2D()(seq_model.outputs[0])
+# --- Klasse
+class_x = keras.layers.GlobalAveragePooling2D()(shared)
+class_x = keras.layers.Dropout(0.3)(class_x)
+class_output = keras.layers.Dense(len(label_names), activation="softmax", name="class")(class_x)
 
-# Verzweigung für zwei Outputs
-class_output = keras.layers.Dense(len(label_names), activation="softmax", name="class")(avg)
-bbox_output = keras.layers.Dense(4, name="bbox")(avg)
+
+# --- BBox
+bbox_x = keras.layers.Conv2D(64, (1, 1), activation='relu')(shared)   # (8,8,1024) → (8,8,64)
+bbox_x = keras.layers.Conv2D(16, (3, 3), activation='relu')(bbox_x)   # (8,8,64)  → (6,6,16)
+bbox_x = keras.layers.Flatten()(bbox_x)                                # → 576
+bbox_x = keras.layers.Dropout(0.3)(bbox_x)
+bbox_x = keras.layers.Dense(64, activation='relu')(bbox_x)             # → 64
+bbox_x = keras.layers.Dropout(0.2)(bbox_x)
+bbox_output = keras.layers.Dense(4, activation='sigmoid', name="bbox")(bbox_x)
 
 
 
@@ -169,15 +172,40 @@ model.summary()
 #### KERAS OPTIMIZER
 optimizer = keras.optimizers.Adam(learning_rate=0.001)  # 0.002
 
-model.compile(loss=["sparse_categorical_crossentropy", "mse"], loss_weights=[0.8, 0.2], optimizer=optimizer, metrics=["accuracy", "r2_score"])
+model.compile(loss=["sparse_categorical_crossentropy", "mse"], loss_weights=[0.2, 0.8], optimizer=optimizer, metrics={"class": "accuracy", "bbox": "r2_score"})
 
 
 
 history = model.fit(X_train, [y_train_class, y_train_bbox], batch_size=32, epochs=300, validation_split=0.2, verbose=1)
 
+# Nach dem Training:
+model.save("mtg_detector.keras")
 
 
+##### AUSWERTUNG
+# Lernkurven - BBox MAE
+plt.plot(history.history['bbox_r2_score'], label='Train')
+plt.plot(history.history['val_bbox_r2_score'], label='Validation')
+plt.xlabel('Epoch')
+plt.ylabel('BBox R2 Score')
+plt.legend()
+plt.show()
 
+# Lernkurven - Class Accuracy
+plt.plot(history.history['class_accuracy'], label='Train')
+plt.plot(history.history['val_class_accuracy'], label='Validation')
+plt.xlabel('Epoch')
+plt.ylabel('Class Accuracy')
+plt.legend()
+plt.show()
+
+# Lernkurven - Loss
+plt.plot(history.history['loss'], label='Train')
+plt.plot(history.history['val_loss'], label='Validation')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
 
 
 
