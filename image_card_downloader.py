1760eb0c61043e1301377f259cc650861649c48d
KKoehrsen <>
2026-03-04 10:40:16 +0100

    fixed clean name

--- image_card_downloader.py
+++ image_card_downloader.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import time
+import unicodedata
 
 set_code = "ltr"  # ice - Ice Age
 
@@ -23,11 +24,15 @@ print(f"{len(all_cards)} Karten gefunden")
 labels = {}
 
 if os.path.exists("label_file.txt"):
-    ### Öffnen der Datei, wenn vorhanden
     with open("label_file.txt") as f:
-        for line in f.readlines():
-            labels.append(line.strip())
+        for i, line in enumerate(f.readlines()):
+            labels[line.strip()] = i
 
+def clean_name(name):
+    # Sonderzeichen normalisieren (ó → o, é → e, etc.)
+    name = unicodedata.normalize("NFKD", name).encode("ascii", "ignore").decode("ascii")
+    name = name.replace(" ", "-").replace("/", "-").replace(",", "").replace("'", "").lower()
+    return name
 
 for i, card in enumerate(all_cards):
     if "image_uris" not in card:
@@ -35,7 +40,7 @@ for i, card in enumerate(all_cards):
 
     img_url = card["image_uris"]["png"]
     name = f"{set_code}-{card['collector_number']}-{card['name']}"
-    name = name.replace(" ", "-").replace("/", "-").replace(",", "").lower()
+    name =clean_name(name)
 
     if name in labels:
         print(f"  [{i+1}/{len(all_cards)}] {name} → ÜBERSPRUNGEN")
